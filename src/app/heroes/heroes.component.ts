140d1089c56845982b6730c820d67baf0021bf8f
Adam Jazairi <jazairi@mit.edu>
2020-05-27 16:47:47 -0400

    Implement messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: "app-heroes",
@@ -8,10 +10,14 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  heroes: Hero[];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
     this.getHeroes();
@@ -19,6 +25,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
